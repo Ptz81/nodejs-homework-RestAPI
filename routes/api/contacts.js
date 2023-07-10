58b4225304aee7183274de51251ecdb776a2f43a
Taras Papka <taras.papka@protonmail.com>
2023-07-10 20:15:58 +0300

    added try catch to router

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -7,28 +7,53 @@ const router = express.Router()
 const { id, name, phone, email } = contacts;
 
 router.get('/', async (req, res, next) => {
-  const result = await contacts.listContacts();
+  try {
+    const result = await contacts.listContacts();
   res.json(result);
+  }catch (error) {
+    console.log(error.message)
+   }
+  
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  const result = await contacts.getContactById(id)
+  try {
+    const result = await contacts.getContactById(id)
   res.json(result);
+  } catch (error) {
+    console.log(error.message)
+   }
+  
 })
 
 router.post('/', async (req, res, next) => {
-   const result = await contacts.addContact(name, email, phone);
+  try {
+    const result = await contacts.addContact(name, email, phone);
   res.json(result)
+  } catch (error) {
+    console.log(error.message)
+   }
+   
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  const result = await contacts.removeContact(id);
+  try {
+    const result = await contacts.removeContact(id);
   res.json(result)
+  } catch (error) {
+    console.log(error.message)
+   }
+  
 })
 
 router.put('/:contactId', async (req, res, next) => {
-   const result = await contacts.updateContact(id, { name, email, phone });
+  try {
+    const result = await contacts.updateContact(id, { name, email, phone });
   res.json(result)
+  } catch (error) {
+    console.log(error.message)
+   }
+   
 })
 
 export default router;
