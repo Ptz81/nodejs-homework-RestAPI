8161e7921ffb256eb3412cce9478899a4d116eae
Taras Papka <taras.papka@protonmail.com>
2023-07-14 22:38:30 +0300

    added mongoose connection

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 import app from "./app.js"
+import mongoose from "mongoose"
 
-app.listen(3000, () => {
+const DB_HOST = 'mongodb+srv://Taras:12345qwert@cluster0.ycalfsm.mongodb.net/Contacts_book?retryWrites=true&w=majority'
+
+mongoose.set('strictQuery', true)
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
+  })
+  .catch(error => {
+    console.log(error.message)
+    process.exit(1)
+  })
+
