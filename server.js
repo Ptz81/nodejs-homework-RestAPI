3044b5978be65ab5d2ec2b706106b2da8790a8c5
Taras Papka <taras.papka@protonmail.com>
2023-07-15 21:18:42 +0300

    added env data

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import app from "./app.js"
 import mongoose from "mongoose"
-import DB_HOST from "./config.js"
+// import DB_HOST from "./config.js"
 
-
+const {DB_HOST} = process.env;
 
 mongoose.set('strictQuery', true)
 
 mongoose.connect(DB_HOST)
   .then(() => {
     app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+  console.log("Database connection successful. Server running. Use our API on port: 3000")
 })
   })
   .catch(error => {
