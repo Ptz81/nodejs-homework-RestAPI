f4173c01308535da29f5c5f31b961a74508873ab
Taras Papka <taras.papka@protonmail.com>
2023-07-22 22:07:41 +0300

    auth schema

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ import cors from 'cors';
 import router from './routes/api/contacts.js';
 import dotenv from 'dotenv'
 
+import authRouter from './routes/api/auth.js'
+
 const app = express()
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 dotenv.config()
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
-
+app.use('/api/auth', authRouter)
 app.use('/api/contacts', router)
 
 app.use((req, res) => {
---
+++ models/user.js
@@ -0,0 +1,46 @@
+import { Schema, model } from "mongoose";
+import Joi from "joi";
+import handleMongooseError from "../helpers/handleMongooseError.js";
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        match: emailRegexp,
+        required: true,
+    },
+    password: {
+        type: String,
+        minlength: 6,
+        required: true,
+    },
+
+}, { versionKey: false, timestamps: true })
+
+userSchema.post("save", handleMongooseError);
+
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min().required(),
+  
+})
+
+const loginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+  
+})
+
+export const User = model("user", userSchema);
+
+export const userSchemaSet = {
+  registerSchema,
+  loginSchema,
+};
