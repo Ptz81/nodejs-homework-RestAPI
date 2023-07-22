5529c16658f85756effa6c37c0905f10613229cf
Taras Papka <taras.papka@protonmail.com>
2023-07-22 22:52:47 +0300

    validation of dublicate error status

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ import { User } from '../models/user.js';
 
 const register = async (req, res) => {
     const newUser = await User.create(req.body);
-    res.json({
+    res.status(201).json({
         email: newUser.email,
         name: newUser.name,
     });
