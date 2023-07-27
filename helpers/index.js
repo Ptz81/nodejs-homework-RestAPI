3d3ba792b749b0c5f2a71e32cb771999fae33b42
Taras Papka <taras.papka@protonmail.com>
2023-07-27 21:05:11 +0300

    made correction of mistake

--- helpers/index.js
+++ helpers/index.js
@@ -1,8 +1,10 @@
 import HttpErrors from "./HttpError.js";
 import ctrlWrapper from "./ctrlWrapper.js"
 import handleMongooseError from "./handleMongooseError.js"
+import isEmptyBody from "./isEmptyBody.js";
 export default {
     HttpErrors,
     ctrlWrapper,
-    handleMongooseError
+    handleMongooseError,
+    isEmptyBody
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,7 +5,8 @@ import {isValidId} from '../../middlewares/isValidId.js';
 import { schemaSet } from '../../models/contact.js';
 import validateBody from '../../middlewares/validateBody.js';
 import authenticate from '../../middlewares/authenticate.js';
-import isEmptyBody from '../../helpers/isEmptyBody.js';
+
+
 
 router.use(authenticate)
 
@@ -13,11 +14,11 @@ router.get('/', ctrl.getAll);
 
 router.get('/:id',isValidId, ctrl.getById);
 
-router.post('/', isEmptyBody, validateBody(schemaSet.addSchema), ctrl.addContact);
+router.post('/', validateBody(schemaSet.addSchema), ctrl.addContact);
 
 router.delete('/:id', isValidId, ctrl.deleteContact);
 
-router.put('/:id', isEmptyBody, isValidId, validateBody(schemaSet.addSchema), ctrl.updateContact);
-router.patch('/:id/favorite', isEmptyBody, isValidId, validateBody(schemaSet.updateFavoriteSchema), ctrl.updateFavorite);
+router.put('/:id', isValidId, validateBody(schemaSet.addSchema), ctrl.updateContact);
+router.patch('/:id/favorite', isValidId, validateBody(schemaSet.updateFavoriteSchema), ctrl.updateFavorite);
 
 export default router;
